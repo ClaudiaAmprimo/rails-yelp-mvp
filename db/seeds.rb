4cd0172bd71aeb42e5b6c5446a9112f488b21fd0
claudiaamprimo <claudia.amprimo@gmail.com>
2023-08-16 16:46:01 +0200

    2 challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+# destroy_all es para que no se creen repetidos cada vez que haga db:seed
+Restaurant.create(name: 'Dishoom', address: '7 Boundary St, London E2', phone_number: '555555555', category: 'chinese')
+Restaurant.create(name: 'Pizza East', address: 'High St, London E1 6PQ', phone_number: '555555555', category: 'italian')
+Restaurant.create(name: 'Mr Sushi', address: 'eixample, Barcelona', phone_number: '555555555', category: 'japanese')
+Restaurant.create(name: 'Oh la la', address: 'borne, Barcelona', phone_number: '555555555', category: 'french')
+Restaurant.create(name: 'Belgamita', address: 'sant cougat, Barcelona', phone_number: '555555555', category: 'belgian')
